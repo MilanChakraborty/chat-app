8ecb88392351af37f18c1cc3709c219030cec53c
Milan Chakraborty <MilanChakraborty@users.noreply.github.com>
2023-08-27 02:16:02 +0530

    add: Adds Users class to Store Username and passwords of multiple users

---
+++ src/models/users.js
@@ -0,0 +1,36 @@
+const { createHash } = require('node:crypto');
+
+class Users {
+  #users;
+  constructor(users = {}) {
+    this.#users = users;
+  }
+
+  #getUserHash(username) {
+    return createHash('md5').update(username).digest('hex');
+  }
+
+  validateUserLogin(username, password) {
+    const userHash = this.#getUserHash(username);
+    const userDetails = this.#users[userHash];
+    if (!userDetails) return false;
+
+    return userDetails.password === password;
+  }
+
+  isUserPresent(userHash) {
+    return userHash in this.#users;
+  }
+
+  addUser(username, password) {
+    const userHash = this.#getUserHash(username);
+    this.#users[userHash] = { username, password };
+    return userHash;
+  }
+}
+
+const createUsers = (usersDetails) => {
+  return new Users(usersDetails);
+};
+
+module.exports = { Users, createUsers };
---
+++ test/models/users-test.js
@@ -0,0 +1,58 @@
+const { describe, it } = require('node:test');
+const assert = require('assert');
+const { Users, createUsers } = require('../../src/models/users');
+
+describe('Users', () => {
+  describe('isUserPresent', () => {
+    it('Should be false if the user doesnot exist', () => {
+      const users = new Users();
+      assert.strictEqual(users.isUserPresent('not-a-hash'), false);
+    });
+
+    it('Should be true if the user exists', () => {
+      const users = new Users({ userHash: {} });
+      assert.strictEqual(users.isUserPresent('userHash'), true);
+    });
+  });
+
+  describe('add User', () => {
+    it('Should be able to add a new user', () => {
+      const users = new Users();
+      const userHash = users.addUser('milan', '1234');
+
+      assert.ok(users.isUserPresent(userHash));
+    });
+  });
+
+  describe('validateUserLogin', () => {
+    it('Should be true if the username and the password are given correctly', () => {
+      const users = new Users();
+      users.addUser('milan', '1234');
+
+      assert.ok(users.validateUserLogin('milan', '1234'));
+    });
+
+    it('Should be false if user doesnot exist', () => {
+      const users = new Users();
+      users.addUser('milan', '1234');
+
+      assert.strictEqual(users.validateUserLogin('raj', '1234'), false);
+    });
+
+    it('Should be false if password is incorrect', () => {
+      const users = new Users();
+      users.addUser('milan', '1234');
+
+      assert.strictEqual(users.validateUserLogin('milan', '123'), false);
+    });
+  });
+});
+
+describe('CreateUser', () => {
+  it('Should create an Users instance with the given data', () => {
+    const usersDetails = { userHash: { username: 'milan', password: 1234 } };
+    const users = createUsers(usersDetails);
+
+    assert.ok(users.isUserPresent('userHash'));
+  });
+});
