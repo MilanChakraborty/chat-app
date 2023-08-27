c9d5b05085a59422a01ccb05fe7f6171222eeea4
Milan Chakraborty <MilanChakraborty@users.noreply.github.com>
2023-08-27 09:25:29 +0530

    untrack: Untracks auth-data.json

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const fs = require('node:fs');
 const { createAndSetupApp } = require('./src/app');
+const { createAuthController } = require('./src/auth-controller');
 
-const AUTH_STORAGE_PATH = './auth-data.js';
+const AUTH_STORAGE_PATH = './auth-data.json';
 
 const injectDependencies = (app) => {
   const authController = createAuthController(AUTH_STORAGE_PATH, fs);
+  app.context = { authController };
 };
 
 const main = () => {
