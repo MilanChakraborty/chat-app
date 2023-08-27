944940868de479f2749c50b1ff28f6fc7cfef62a
Milan Chakraborty <MilanChakraborty@users.noreply.github.com>
2023-08-27 15:57:39 +0530

    add: Adds Input Controller which can add events to the send and connect form

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -2,8 +2,9 @@ const main = () => {
   const authContainer = document.querySelector('.auth');
   const view = new View(authContainer);
   const chatService = new ChatService();
+  const inputController = new InputController();
 
-  const controller = new Controller(chatService, view);
+  const controller = new Controller(chatService, view, inputController);
   controller.start();
 };
 
--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -1,9 +1,47 @@
+class InputController {
+  #readConnectInputBox() {
+    const connectInputBox = document.querySelector('#connect-input-box');
+    const connectTo = connectInputBox.value;
+    connectInputBox.value = '';
+
+    return connectTo;
+  }
+
+  #readSendInputBox() {
+    const sendInputBox = document.querySelector('#send-input-box');
+    const message = sendInputBox.value;
+    sendInputBox.value = '';
+
+    return message;
+  }
+
+  onConnect(onSubmit) {
+    const connectForm = document.querySelector('#connection-form');
+    connectForm.onsubmit = (event) => {
+      event.preventDefault();
+      const connectTo = this.#readConnectInputBox();
+      onSubmit(connectTo);
+    };
+  }
+
+  onSend(onSubmit) {
+    const sendForm = document.querySelector('#send-form');
+    sendForm.onsubmit = (event) => {
+      event.preventDefault();
+      const message = this.#readSendInputBox();
+      onSubmit(message);
+    };
+  }
+}
+
 class Controller {
   #view;
   #chatService;
-  constructor(chatService, view) {
+  #inputController;
+  constructor(chatService, view, inputController) {
     this.#chatService = chatService;
     this.#view = view;
+    this.#inputController = inputController;
   }
 
   #fetchAndRenderLoginDetails() {
@@ -19,5 +57,7 @@ class Controller {
   start() {
     this.#fetchAndRenderLoginDetails();
     this.#view.addListener('logout', () => this.#requestLogout('logout'));
+    this.#inputController.onConnect((connectTo) => console.log(connectTo));
+    this.#inputController.onSend((message) => console.log(message));
   }
 }
