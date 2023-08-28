730a5832aa7d38588eb2b5476a2bf247a4cdfe6f
Milan Chakraborty <MilanChakraborty@users.noreply.github.com>
2023-08-28 16:47:00 +0530

    add: adds signup page, signup page doesnt send any response to the server

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -14,28 +14,29 @@ const readPassword = () => {
   return password;
 };
 
-const sendLoginRequest = (event) => {
+const sendSignupRequest = (event) => {
   event.preventDefault();
   const username = readUsername();
   const password = readPassword();
+  console.log(username, password);
 
-  fetch('/login', {
-    method: 'POST',
-    headers: { 'content-type': 'application/json' },
-    body: JSON.stringify({ username, password }),
-  })
-    .then((res) => {
-      if (res.redirected) {
-        window.location.replace(res.url);
-        return;
-      }
-      alert('Password Entered is Wrong');
-    })
-    .catch((err) => err);
+  // fetch('/login', {
+  //   method: 'POST',
+  //   headers: { 'content-type': 'application/json' },
+  //   body: JSON.stringify({ username, password }),
+  // })
+  //   .then((res) => {
+  //     if (res.redirected) {
+  //       window.location.replace(res.url);
+  //       return;
+  //     }
+  //     alert('Password Entered is Wrong');
+  //   })
+  //   .catch((err) => err);
 };
 
 const setupLoginForm = () => {
-  const formElement = document.querySelector('#login-form');
+  const formElement = document.querySelector('#signup-form');
   formElement.onsubmit = sendSignupRequest;
 };
 
